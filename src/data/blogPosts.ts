cc56ab8f7b8f1041c9adecba8ada56ae794d5c8f
Kyuwon Kim <kyuwonkim106@gmail.com>
2026-03-19 22:16:26 +0900

    Update blogPosts.ts

--- src/data/blogPosts.ts
+++ src/data/blogPosts.ts
@@ -9,6 +9,35 @@ export interface BlogPost {
 
 export const blogPosts: BlogPost[] = [
   {
+    id: "post2",
+    title: "What made me step into academia?",
+    date: "2026-03-18",
+    excerpt: "I didn’t plan to become a researcher. One moment just made me see things differently.",
+    content: `
+I always thought I would become a teacher.
+
+From a very young age through my undergraduate years, that idea never changed. I think it was because I wanted to be someone who helps students, someone who makes things a little clearer.
+
+Then, during my undergraduate, I attended a conference with funding from my department.  
+I sat in on a session without expecting much, but it stayed with me.
+
+It made me realize that research grows through small pieces of work; each study modest on its own, but together shaping something larger.
+That was when I started to see research differently.
+
+Teaching helps the students in front of me.  
+Research, I felt, could help shape something a bit larger. Something that lasts longer and reaches further.
+
+Only later did I learn that this way of thinking aligns with Design-Based Research,
+that knowledge develops iteratively, through accumulation.
+
+But at that time, I didn’t have the words for it.
+I just knew that I wanted to be part of that process.
+`,
+
+    tags: ["Dream", "Research"],
+  },
+  
+  {
     id: "post1",
     title: "Things I Love",
     date: "2026-02-18",
